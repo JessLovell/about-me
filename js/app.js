010b8ba6b084335f33aac2865a74c40f3305c30a
Jessica Lovell <jessicaamberlovell@gmail.com>
2018-08-07 19:46:46 -0700

    updates to CSS, it's v basic

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@
 
 //Get the user's name 
 var username = prompt('Hello, I\'m Jessica, welcome to my guessing game. What is your name?');
-alert('nice to have you here, ' +  username + ' these are all Yes/No questions, so feel free to answer wtih y/n or yes/no. ');
+alert('Nice to have you here, ' +  username + ' these are all Yes/No questions, so feel free to answer wtih y/n or yes/no. ');
 
 //An hash to hold questions and values 
-var questions = {'Am I a Seattle native? ': 'no', 'Have I been to Vermont?': 'no', 'Is Blue Bell my favorite Ice Cream brand?': 'yes', 'Do I have any siblings?': 'yes', 'last question?': 'yes'};
+var questions = {'Am I a Seattle native? ': 'no', 'Have I been to Vermont?': 'no', 'Is Blue Bell my favorite Ice Cream brand?': 'yes', 'Do I have any siblings?': 'yes', 'Am I a dog lover?': 'yes'};
 
 
 for (var i = 0; i < Object.keys(questions).length; i++){
     var userInput = prompt(Object.keys(questions)[i]).toLowerCase();
-    console.log(Object.values(questions)[i]);
-    console.log(userInput);
+    console.log('The user answered: ' + userInput + ' to ' + Object.keys(questions)[i]);
     
     var values = Object.values(questions)[i];
     //check the input against the valued pair **need to get single character check*
@@ -19,6 +18,7 @@ for (var i = 0; i < Object.keys(questions).length; i++){
         alert('Correct!');
     } 
     else{
-        alert('And I thought we were friends!');
+        var errorMsg = ['And I thought we were friends!', 'Nope.', 'Wrong-o!'];
+        alert(errorMsg[Math.floor(Math.random()*2)]);
     }
 }
